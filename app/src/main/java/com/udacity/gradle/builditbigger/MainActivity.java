51350b7f87f271e2319aa8f5e39f0ab1b10710fe
Vachan <vachaneagleman@gmail.com>
2018-11-15 23:08:05 +0530

    minor fix

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -18,6 +18,8 @@ public class MainActivity extends AppCompatActivity implements MainActivityFragm
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new EndpointAsyncTask().execute(this);
     }
 
 
